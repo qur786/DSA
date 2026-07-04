999ad48dcea9ae23cf81d3bb60804034b33a0ba4
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-04 10:39:40 +0530

    Add solution for 148. Sort List

---
+++ 148. Sort List/C++/solution-3.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* splitMiddle(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode *slow = head, *fast = head->next->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* middle = slow->next;
+        slow->next = nullptr;
+
+        return middle;
+    }
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
+        ListNode* result = new ListNode();
+        ListNode* curr = result;
+
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+
+            curr = curr->next;
+        }
+
+        if (list1)
+            curr->next = list1;
+        if (list2)
+            curr->next = list2;
+
+        ListNode* dummy = result;
+        result = result->next;
+
+        delete dummy;
+
+        return result;
+    }
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode* middle = splitMiddle(head);
+        ListNode* list1 = sortList(head);
+        ListNode* list2 = sortList(middle);
+        ListNode* result = merge(list1, list2);
+
+        return result;
+    }
+};
