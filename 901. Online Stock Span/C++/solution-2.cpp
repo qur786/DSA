c405e00de84c12a80e4739f7d9d553e682329341
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-15 00:07:30 +0530

    Add solution for 901. Online Stock Span

---
+++ 901. Online Stock Span/C++/solution-2.cpp
@@ -0,0 +1,27 @@
+class StockSpanner {
+public:
+    vector<int> prices;
+    stack<int> st;
+    StockSpanner() {}
+
+    int next(int price) {
+        int i = prices.size();
+        while (!st.empty() && prices[st.top()] <= price) {
+            st.pop();
+        }
+        int value;
+        if (!st.empty())
+            value = i - st.top();
+        else
+            value = i + 1;
+        st.push(i);
+        prices.push_back(price);
+        return value;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
