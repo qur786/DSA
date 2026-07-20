03533e9ef7e3807211f6bce2a4978f9f115f750a
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-20 14:38:55 +0530

    Add solution for 42. Trapping Rain Water

---
+++ 42. Trapping Rain Water/C++/solution-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftMax = INT_MIN, rightMax = INT_MIN;
+        int left = 0, right = height.size() - 1;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                leftMax = max(leftMax, height[left]);
+                totalWater += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = max(rightMax, height[right]);
+                totalWater += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+};
