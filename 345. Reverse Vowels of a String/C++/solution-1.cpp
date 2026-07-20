baeca1c92aaa379bfbab6b55553352f0aaa14311
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-20 21:57:57 +0530

    Add solution for 345. Reverse Vowels of a String

---
+++ 345. Reverse Vowels of a String/C++/solution-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int left = 0, right = s.size() - 1;
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u',
+                                      'A', 'E', 'I', 'O', 'U'};
+
+        while (left < right) {
+            while (left < right && !vowels.count(s[left]))
+                left++;
+            while (left < right && !vowels.count(s[right]))
+                right--;
+            if (left >= right)
+                break;
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+
+        return s;
+    }
+};
