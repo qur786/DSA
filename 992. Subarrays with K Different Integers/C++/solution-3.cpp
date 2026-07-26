5f44037bd52b5b52c1f70d34a924630f3abb5b75
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-26 23:44:42 +0530

    Add solution for 992. Subarrays with K Different Integers

---
+++ 992. Subarrays with K Different Integers/C++/solution-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subArraysWithAtMostK(vector<int>& nums, int k) {
+        int left = 0, size = nums.size(), subArrayCount = 0;
+        unordered_map<int, int> numCount;
+
+        for (int right = 0; right < size; right++) {
+            numCount[nums[right]]++;
+            while (numCount.size() > k) {
+                numCount[nums[left]]--;
+                if (numCount[nums[left]] == 0)
+                    numCount.erase(nums[left]);
+                left++;
+            }
+            subArrayCount += right - left + 1;
+        }
+        return subArrayCount;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return subArraysWithAtMostK(nums, k) -
+               subArraysWithAtMostK(nums, k - 1);
+    }
+};
