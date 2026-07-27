8c9fdd671a67fce49b14fbe91016095b9e7b7cf0
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-07-27 23:12:14 +0530

    Add solution for 2. Add Two Numbers

---
+++ 2. Add Two Numbers/C++/solution-3.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+
+        while (l1 && l2) {
+            int sum = l1->val + l2->val + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            l1 = l1->next;
+            l2 = l2->next;
+            curr = curr->next;
+        }
+        while (l1) {
+            int sum = l1->val + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            l1 = l1->next;
+            curr = curr->next;
+        }
+        while (l2) {
+            int sum = l2->val + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            l2 = l2->next;
+            curr = curr->next;
+        }
+
+        if (carry > 0)
+            curr->next = new ListNode(carry);
+
+        ListNode* result = dummy->next;
+        delete dummy;
+
+        return result;
+    }
+};
