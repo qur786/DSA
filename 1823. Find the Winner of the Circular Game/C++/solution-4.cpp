822b97743cec5a29af8e601f252a83b4416a090f
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-06 16:38:03 +0530

    Add solution for 1823. Find the Winner of the Circular Game

---
+++ 1823. Find the Winner of the Circular Game/C++/solution-4.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        if (n == 1)
+            return 1;
+
+        int survivor = (findTheWinner(n - 1, k) - 1 + k) % n + 1;
+        return survivor;
+    }
+};
