c2edebdd52a73e8e035ca9844c154ed9e1e81ad4
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-08 18:22:50 +0530

    Add solution for 1670. Design Front Middle Back Queue

---
+++ 1670. Design Front Middle Back Queue/C++/solution-2.cpp
@@ -0,0 +1,80 @@
+class FrontMiddleBackQueue {
+private:
+    deque<int> left, right;
+    void balance() {
+        if (this->left.size() > (this->right.size() + 1)) {
+            this->right.push_front(this->left.back());
+            this->left.pop_back();
+        } else if (this->left.size() < this->right.size()) {
+            this->left.push_back(this->right.front());
+            this->right.pop_front();
+        }
+    }
+    bool empty() { return this->left.empty() && this->right.empty(); }
+
+public:
+    FrontMiddleBackQueue() {}
+
+    void pushFront(int val) {
+        this->left.push_front(val);
+        this->balance();
+    }
+
+    void pushMiddle(int val) {
+        if (this->left.size() > this->right.size()) {
+            this->right.push_front(this->left.back());
+            this->left.pop_back();
+        }
+
+        this->left.push_back(val);
+    }
+
+    void pushBack(int val) {
+        this->right.push_back(val);
+        this->balance();
+    }
+
+    int popFront() {
+        if (this->empty())
+            return -1;
+        int value = this->left.front();
+        this->left.pop_front();
+        this->balance();
+        return value;
+    }
+
+    int popMiddle() {
+        if (this->empty())
+            return -1;
+        int value = this->left.back();
+        this->left.pop_back();
+        this->balance();
+        return value;
+    }
+
+    int popBack() {
+        if (this->empty())
+            return -1;
+        int value;
+        if (this->right.empty()) {
+            value = this->left.back();
+            this->left.pop_back();
+        } else {
+            value = this->right.back();
+            this->right.pop_back();
+        }
+        this->balance();
+        return value;
+    }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
