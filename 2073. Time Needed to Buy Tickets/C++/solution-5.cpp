ca0563b2d89f036dd42cf6aee9add558c0b46628
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-08 12:22:06 +0530

    Add solution for 2073. Time Needed to Buy Tickets

---
+++ 2073. Time Needed to Buy Tickets/C++/solution-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int size = tickets.size(), timer = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (i <= k) {
+                timer += min(tickets[i], tickets[k]);
+            } else {
+                timer += min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return timer;
+    }
+};
