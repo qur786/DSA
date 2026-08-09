a7bfd6e41dc6ecd7b73949ec28357a88938a5b03
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-09 13:03:28 +0530

    Add solution for 239. Sliding Window Maximum

---
+++ 239. Sliding Window Maximum/C++/solution-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        int size = nums.size();
+        vector<int> result;
+        result.reserve(size - k + 1);
+
+        for (int i = 0; i < size; i++) {
+            while (!dq.empty() && (i - dq.front()) >= k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            if (i >= (k - 1))
+                result.push_back(nums[dq.front()]);
+        }
+
+        return result;
+    }
+};
