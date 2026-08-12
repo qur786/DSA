f857a75dbc5877f35ae32a21278c733c5ef480d9
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-12 22:22:12 +0530

    Add solution for LeetCode/525. Contiguous Array

---
+++ LeetCode/525. Contiguous Array/C++/solution-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int size = nums.size();
+        int maxLen = 0;
+        int prefixSum = 0;
+        unordered_map<int, int> sumMap = {{0, -1}};
+
+        for (int i = 0; i < size; i++) {
+            prefixSum += nums[i] ? 1 : -1;
+
+            if (sumMap.count(prefixSum))
+                maxLen = max(maxLen, i - sumMap[prefixSum]);
+            else
+                sumMap[prefixSum] = i;
+        }
+
+        return maxLen;
+    }
+};
