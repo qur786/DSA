2c4ebb47f0fe1de9fc2962734a2655c1af015fb6
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-16 15:27:17 +0530

    Add solution for LeetCode/918. Maximum Sum Circular Subarray

---
+++ LeetCode/918. Maximum Sum Circular Subarray/C++/solution-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int size = nums.size();
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        int newSize = nums.size();
+        vector<long long> prefixSum(newSize + 1, 0);
+        long long maxSum = LLONG_MIN;
+        deque<int> dq;
+
+        for (int i = 1; i <= newSize; i++)
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+
+        for (int i = 0; i <= newSize; i++) {
+            while (!dq.empty() && (i - dq.front()) > size)
+                dq.pop_front();
+            if (!dq.empty())
+                maxSum = max(maxSum, prefixSum[i] - prefixSum[dq.front()]);
+            while (!dq.empty() && (prefixSum[i] <= prefixSum[dq.back()]))
+                dq.pop_back();
+            dq.push_back(i);
+        }
+
+        return maxSum;
+    }
+};
