45852ea110c5e674854f4c23606fc6ba11341690
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-18 15:40:23 +0530

    Add solution for LeetCode/1926. Nearest Exit from Entrance in Maze

---
+++ LeetCode/1926. Nearest Exit from Entrance in Maze/C++/solution-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int rows = maze.size(), cols = maze[0].size();
+        queue<pair<int, int>> nodes;
+        const int rowD[4] = {1, -1, 0, 0};
+        const int colD[4] = {0, 0, 1, -1};
+        nodes.emplace(entrance[0], entrance[1]);
+        maze[entrance[0]][entrance[1]] = '+';
+        int distance = 0;
+
+        while (!nodes.empty()) {
+            int size = nodes.size();
+
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = nodes.front();
+                nodes.pop();
+
+                for (int d = 0; d < 4; d++) {
+                    int adjX = x + rowD[d];
+                    int adjY = y + colD[d];
+
+                    if (0 <= adjX && adjX < rows && 0 <= adjY && adjY < cols) {
+                        if (maze[adjX][adjY] == '.') {
+                            maze[adjX][adjY] = '+';
+                            nodes.emplace(adjX, adjY);
+                        }
+                    } else if (x != entrance[0] || y != entrance[1])
+                        return distance;
+                }
+            }
+            distance += 1;
+        }
+
+        return -1;
+    }
+};
