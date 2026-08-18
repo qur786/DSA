5f00866e8aaf26d7906f324c16f6f6fc65cb0af5
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-18 14:09:25 +0530

    Add solution for LeetCode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ LeetCode/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/C++/solution-8.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int size = nums.size();
+        deque<int> incr, decr;
+        int maxLen = 0, left = 0;
+
+        for (int right = 0; right < size; right++) {
+            while (!incr.empty() && nums[right] < incr.back())
+                incr.pop_back();
+            incr.push_back(nums[right]);
+            while (!decr.empty() && nums[right] > decr.back())
+                decr.pop_back();
+            decr.push_back(nums[right]);
+            while (!incr.empty() && !decr.empty() &&
+                   (decr.front() - incr.front()) > limit) {
+                if (nums[left] == incr.front())
+                    incr.pop_front();
+                if (nums[left] == decr.front())
+                    decr.pop_front();
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
