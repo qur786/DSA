ba73b73815be4e2a6171532546b44d9bb2c0fc35
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-19 19:36:26 +0530

    Add solution for LeetCode/542. 01 Matrix

---
+++ LeetCode/542. 01 Matrix/C++/solution-2.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size(), cols = mat[0].size();
+        vector<vector<int>> answer(rows, vector<int>(cols, 0));
+        queue<pair<int, int>> nodes;
+        int ones = 0;
+        const int rowD[4] = {0, 0, 1, -1};
+        const int colD[4] = {1, -1, 0, 0};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (!mat[i][j])
+                    nodes.emplace(i, j);
+                else
+                    ones++;
+            }
+        }
+
+        if (!ones)
+            return answer;
+
+        int distance = 1;
+
+        while (!nodes.empty()) {
+            int size = nodes.size();
+
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = nodes.front();
+                nodes.pop();
+
+                for (int d = 0; d < 4; d++) {
+                    int adjX = x + rowD[d];
+                    int adjY = y + colD[d];
+
+                    if (0 <= adjX && adjX < rows && 0 <= adjY && adjY < cols) {
+                        if (mat[adjX][adjY]) {
+                            mat[adjX][adjY] = 0;
+                            answer[adjX][adjY] = distance;
+                            nodes.emplace(adjX, adjY);
+                        }
+                    }
+                }
+            }
+
+            distance++;
+        }
+
+        return answer;
+    }
+};
