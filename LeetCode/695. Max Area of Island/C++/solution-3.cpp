4e9188194b6aa50c6de570fb635d512a5a30fd88
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-21 08:31:32 +0530

    Add solution for LeetCode/695. Max Area of Island

---
+++ LeetCode/695. Max Area of Island/C++/solution-3.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        int maxArea = 0;
+
+        const int rowD[4] = {0, 0, 1, -1};
+        const int colD[4] = {1, -1, 0, 0};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j]) {
+                    queue<pair<int, int>> nodes;
+                    nodes.emplace(i, j);
+                    grid[i][j] = 0;
+                    int area = 1;
+
+                    while (!nodes.empty()) {
+                        auto [x, y] = nodes.front();
+                        nodes.pop();
+
+                        for (int d = 0; d < 4; d++) {
+                            int adjX = x + rowD[d];
+                            int adjY = y + colD[d];
+
+                            if (0 <= adjX && adjX < rows && 0 <= adjY &&
+                                adjY < cols && grid[adjX][adjY]) {
+                                grid[adjX][adjY] = 0;
+                                area++;
+                                nodes.emplace(adjX, adjY);
+                            }
+                        }
+                    }
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
