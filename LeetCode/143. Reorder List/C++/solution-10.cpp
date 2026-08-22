841a6d8e9ac11888e23843689e1b86fb4cdd7dcc
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-22 18:42:14 +0530

    Add solution for LeetCode/143. Reorder List

---
+++ LeetCode/143. Reorder List/C++/solution-10.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* reverse(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode *curr = head, *prev = nullptr, *next = nullptr;
+
+        while (curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+    ListNode* splitMiddle(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+
+        ListNode *slow = head, *fast = slow->next->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* middle = slow->next;
+        slow->next = nullptr;
+
+        return middle;
+    }
+    void merge(ListNode* list1, ListNode* list2) {
+        if (!list1 || !list2)
+            return;
+
+        while (list2) {
+            ListNode* temp = list1->next;
+            list1->next = list2;
+            list1 = list1->next;
+            list2 = temp;
+        }
+    }
+
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next)
+            return;
+        ListNode* middle = splitMiddle(head);
+        ListNode* reverseMiddle = reverse(middle);
+        merge(head, reverseMiddle);
+    }
+};
