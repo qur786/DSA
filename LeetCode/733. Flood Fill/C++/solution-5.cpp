cfbc87fd1f9c8d49b544d2f3c7cdb075fbcc01d8
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-24 19:07:55 +0530

    Add solution for LeetCode/733. Flood Fill

---
+++ LeetCode/733. Flood Fill/C++/solution-5.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+        if (image[sr][sc] == color)
+            return image;
+
+        int rows = image.size(), cols = image[0].size();
+        const int rowD[4] = {0, 0, 1, -1};
+        const int colD[4] = {1, -1, 0, 0};
+        queue<pair<int, int>> nodes;
+        nodes.emplace(sr, sc);
+        int currentColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        while (!nodes.empty()) {
+            auto [x, y] = nodes.front();
+            nodes.pop();
+
+            for (int d = 0; d < 4; d++) {
+                int adjX = x + rowD[d];
+                int adjY = y + colD[d];
+
+                if (0 <= adjX && adjX < rows && 0 <= adjY && adjY < cols &&
+                    image[adjX][adjY] == currentColor) {
+                    image[adjX][adjY] = color;
+                    nodes.emplace(adjX, adjY);
+                }
+            }
+        }
+
+        return image;
+    }
+};
