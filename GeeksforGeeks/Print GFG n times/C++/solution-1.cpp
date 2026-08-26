a4bedf0eef66a91b6e55660686cd04ce247acc21
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-26 12:35:04 +0530

    Add solution for GeeksforGeeks/Print GFG n times

---
+++ GeeksforGeeks/Print GFG n times/C++/solution-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void print(int n) {
+    if (n == 0) return;
+    cout << "GFG ";
+    n--;
+    print(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    print(n);
+    
+
+    return 0;
+}
