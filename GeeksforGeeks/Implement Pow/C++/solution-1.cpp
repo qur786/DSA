9806240d79783986c20551c2819567a2e0c8246e
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-26 14:28:12 +0530

    Add solution for GeeksforGeeks/Implement Pow

---
+++ GeeksforGeeks/Implement Pow/C++/solution-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+	public:
+	double power(double b, int e) {
+	    if (b == 1) return 1;
+		if (e == 0)
+			return 1;
+		if (e == 1)
+			return b;
+		if (e < 0)
+			return power(1/b, -e);
+			
+		double half = power(b, e / 2);
+		
+		return e % 2 == 0 ? half * half : half * half * b;
+	}
+};
