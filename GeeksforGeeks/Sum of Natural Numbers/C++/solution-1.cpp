c452dab09161122708b8e0c19e7e893c7dd9ef25
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-26 12:39:35 +0530

    Add solution for GeeksforGeeks/Sum of Natural Numbers

---
+++ GeeksforGeeks/Sum of Natural Numbers/C++/solution-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+    if (n == 0) return 0;
+    
+    return sum(n - 1) + n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int s = sum(n);
+    cout << s;
+    return 0;
+}
