2f8ff305de557cfa6d1822d3cce4904f51b83f47
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-27 15:41:04 +0530

    Add solution for GeeksforGeeks/Reverse a Linked List

---
+++ GeeksforGeeks/Reverse a Linked List/C++/solution-4.cpp
@@ -0,0 +1,26 @@
+/* Structure of Linked List Node
+class Node {
+	public:
+	int data ;
+	Node *next ;
+	
+	Node(int x) {
+		data = x ;
+		next = nullptr ;
+	}
+};
+*/
+
+class Solution {
+	public:
+	Node* reverseList(Node* head) {
+		if (!head || !head -> next)
+			return head;
+		
+		Node* reverseHead = reverseList(head -> next);
+		head -> next -> next = head;
+		head -> next = nullptr;
+		
+		return reverseHead;
+	}
+};
