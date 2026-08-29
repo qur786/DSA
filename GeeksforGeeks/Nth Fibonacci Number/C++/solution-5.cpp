24bb89d05ec9575afe05df266dc64c503e499b48
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-29 11:47:11 +0530

    Add solution for GeeksforGeeks/Nth Fibonacci Number

---
+++ GeeksforGeeks/Nth Fibonacci Number/C++/solution-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+	public:
+	int nthFibonacci(int n) {
+		int fib1 = 0;
+		int fib2 = 1;
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		int fib3 = 0;
+		
+		for (int i = 2; i <= n; i++) {
+			fib3 = fib1 + fib2;
+			fib1 = fib2;
+			fib2 = fib3;
+		}
+		
+		return fib3;
+	}
+};
