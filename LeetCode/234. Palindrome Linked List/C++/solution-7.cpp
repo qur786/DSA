57b814b39cd4f7854e963271110fdd774d4cef98
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-30 19:22:35 +0530

    Add solution for LeetCode/234. Palindrome Linked List

---
+++ LeetCode/234. Palindrome Linked List/C++/solution-7.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* masterHead = nullptr;
+    bool checkPalindrome(ListNode* head) {
+        if (!head)
+            return true;
+
+        if (!checkPalindrome(head->next))
+            return false;
+        if (!masterHead)
+            return true;
+        if (masterHead->val != head->val)
+            return false;
+        if (masterHead == head || masterHead == head->next) {
+            masterHead = nullptr;
+            return true;
+        }
+
+        masterHead = masterHead->next;
+
+        return true;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        masterHead = head;
+        return checkPalindrome(head);
+    }
+};
