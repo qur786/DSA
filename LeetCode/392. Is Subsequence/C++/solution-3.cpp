a64f6fb6408d0fd943e472773291a0e913017190
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-08-31 14:56:46 +0530

    Add solution for LeetCode/392. Is Subsequence

---
+++ LeetCode/392. Is Subsequence/C++/solution-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    bool isSub(string& s, string& t, int i, int j) {
+        if (i == s.size())
+            return true;
+        if (j == t.size())
+            return false;
+
+        if (s[i] == t[j])
+            return isSub(s, t, i + 1, j + 1);
+
+        return isSub(s, t, i, j + 1);
+    }
+
+public:
+    bool isSubsequence(string s, string t) { return isSub(s, t, 0, 0); }
+};
