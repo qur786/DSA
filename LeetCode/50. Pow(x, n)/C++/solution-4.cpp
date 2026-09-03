dea80165378873bfe42d7a272a2e1f04d88ffd91
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-03 09:57:04 +0530

    Add solution for LeetCode/50. Pow(x, n)

---
+++ LeetCode/50. Pow(x, n)/C++/solution-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0)
+            return 0;
+        if (x == 1)
+            return 1;
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if (n == INT_MIN)
+            return x * myPow(x, n + 1);
+        if (n < 0)
+            return myPow(1 / x, -n);
+
+        double half = myPow(x, n / 2);
+
+        return n % 2 == 0 ? half * half : x * half * half;
+    }
+};
