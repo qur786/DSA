bdae73569c4914f85354c1e39488c384a70ef156
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-03 09:41:51 +0530

    Add solution for GeeksforGeeks/Tower Of Hanoi

---
+++ GeeksforGeeks/Tower Of Hanoi/C++/solution-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+	private:
+	int count = 0;
+	public:
+	int towerOfHanoi(int n, int from, int to, int aux) {
+		// code here
+		if (n == 0)
+			return 0;
+		this -> count++;
+		towerOfHanoi(n - 1, from, aux, to);
+		towerOfHanoi(n - 1, aux, to, from);
+		
+		return this -> count;
+	}
+};
