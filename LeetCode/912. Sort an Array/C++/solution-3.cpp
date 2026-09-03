45a8b466ec3d192c1e6e2ccec46112d6d7e73ee0
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-03 09:46:50 +0530

    Add solution for LeetCode/912. Sort an Array

---
+++ LeetCode/912. Sort an Array/C++/solution-3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if (left >= right)
+            return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        int size1 = mid - left + 1, size2 = right - mid;
+        vector<int> nums1(nums.begin() + left, nums.begin() + mid + 1),
+            nums2(nums.begin() + mid + 1, nums.begin() + right + 1);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < size1 && j < size2) {
+            if (nums1[i] <= nums2[j]) {
+                nums[k] = nums1[i];
+                i++;
+            } else {
+                nums[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < size1) {
+            nums[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j < size2) {
+            nums[k] = nums2[j];
+            j++;
+            k++;
+        }
+    }
+
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+
+        return nums;
+    }
+};
