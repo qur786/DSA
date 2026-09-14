ecffbda0dbfcac741604a2aba1b7bf75fd60cb96
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-14 09:16:40 +0530

    Add solution for LeetCode/463. Island Perimeter

---
+++ LeetCode/463. Island Perimeter/C++/solution-6.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        int perimeter = 0;
+        queue<pair<int, int>> nodes;
+
+        const int rowD[4] = {0, 0, 1, -1};
+        const int colD[4] = {1, -1, 0, 0};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j]) {
+                    nodes.emplace(i, j);
+                }
+            }
+        }
+
+        while (!nodes.empty()) {
+            auto [x, y] = nodes.front();
+            nodes.pop();
+
+            for (int d = 0; d < 4; d++) {
+                int adjX = x + rowD[d];
+                int adjY = y + colD[d];
+
+                if (0 <= adjX && adjX < rows && 0 <= adjY && adjY < cols) {
+                    if (!grid[adjX][adjY]) {
+                        perimeter++;
+                    }
+                } else
+                    perimeter++;
+            }
+        }
+
+        return perimeter;
+    }
+};
