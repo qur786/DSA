074a5b7b5a83e884a75b5f2b8944b1f82a19687d
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-14 09:25:43 +0530

    Add solution for LeetCode/1091. Shortest Path in Binary Matrix

---
+++ LeetCode/1091. Shortest Path in Binary Matrix/C++/solution-6.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        if (grid[0][0] || grid[rows - 1][cols - 1])
+            return -1;
+        if (rows == 1 || cols == 1)
+            return 1;
+        queue<pair<int, int>> nodes;
+        nodes.emplace(0, 0);
+        grid[0][0] = 1;
+        const int rowD[8] = {0, 0, 1, -1, -1, -1, 1, 1};
+        const int colD[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+        int distance = 0;
+
+        while (!nodes.empty()) {
+            distance++;
+            int size = nodes.size();
+
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = nodes.front();
+                nodes.pop();
+
+                for (int d = 0; d < 8; d++) {
+                    int adjX = x + rowD[d];
+                    int adjY = y + colD[d];
+
+                    if (0 <= adjX && adjX < rows && 0 <= adjY && adjY < cols &&
+                        !grid[adjX][adjY]) {
+                        if (rows - 1 == adjX && cols - 1 == adjY)
+                            return distance + 1;
+                        grid[adjX][adjY] = 1;
+                        nodes.emplace(adjX, adjY);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
