6cf0ba4a9f65bd55cab825985d8050fc4c2c38a3
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-24 23:32:45 +0530

    Add solution for LeetCode/3550. Smallest Index With Digit Sum Equal to Index

---
+++ LeetCode/3550. Smallest Index With Digit Sum Equal to Index/C++/solution-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int getDigitSum(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+public:
+    int smallestIndex(vector<int>& nums) {
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (i == getDigitSum(nums[i]))
+                return i;
+        }
+
+        return -1;
+    }
+};
