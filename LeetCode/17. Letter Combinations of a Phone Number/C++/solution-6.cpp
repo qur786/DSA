dfd86aad30a6c3a40857ec69154c942597904bff
QURBAN AHMAD <79472606+qur786@users.noreply.github.com>
2026-09-24 23:23:39 +0530

    Add solution for LeetCode/17. Letter Combinations of a Phone Number

---
+++ LeetCode/17. Letter Combinations of a Phone Number/C++/solution-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    void createComb(vector<string>& answer, string& comb, string& digits,
+                    unordered_map<char, string>& chars, int index, int size) {
+        if (comb.size() == digits.size()) {
+            answer.push_back(comb);
+            return;
+        }
+
+        for (int i = index; i < size; i++) {
+            for (char letter : chars[digits[i]]) {
+                comb.push_back(letter);
+                createComb(answer, comb, digits, chars, i + 1, size);
+                comb.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> answer;
+        string comb;
+        unordered_map<char, string> chars = {
+            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"},
+        };
+        int size = digits.size();
+        createComb(answer, comb, digits, chars, 0, size);
+
+        return answer;
+    }
+};
